7d9c4f7d442416864163a032ce17c7b8f5060039
Daniel Wong <danielwong@Daniels-Mac-mini.local>
2024-03-27 17:33:13 +0800

    Fixed search params bugs

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -7,6 +7,7 @@ import Form from '@components/Form';
 
 const EditPrompt = () => {
     const router = useRouter();
+    const searchParams = useSearchParams();
     const promptId = SearchParams.get('id');
 
     const [submitting, setSubmitting] = useState(false);
