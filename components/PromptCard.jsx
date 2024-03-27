690ad3631a8c6b50e94547fbd30c818c38e65ae9
Daniel Wong <danielwong@Daniels-Mac-mini.local>
2024-03-27 14:57:43 +0800

    Edited PromptCard component and added profile API endpoint

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,6 +7,12 @@ import { usePathname, useRouter } from 'next/navigation';
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState("");
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(false), 3000);
+  };
   
   return (
     <div className="prompt_card">
@@ -24,16 +30,25 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             <h3 className="font-satoshi font-semibold text-gray-900">{post.creator.username}</h3>
             <p className="font-inter text-sm text-gray-500">{post.creator.email}</p>
           </div>
+        </div>
 
-          <div className='copy-btn' onClick={() => {}}>
+        <div className='copy-btn' onClick={handleCopy}>
             <Image 
               src={copied === post.prompt ? '/assets/icons/tick.svg' : '/assets/icons/copy.svg'}
+              alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
               width={12}
               height={12}
             />
-          </div>
         </div>
       </div>
+
+      <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
+      <p 
+        className='font-inter text-sm blue_gradient cursor_pointer' 
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >
+        {post.tag}
+      </p>
     </div>
   )
 }
